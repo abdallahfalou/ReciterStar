39d9c3f7b0a772d1a1194aa43c1e8de149334df8
Abdallah El-Falou <abdallah.elfalou@gmail.com>
2014-02-04 03:03:01 -0500

    move to brace.io for mic to work

--- javascript/custom2.js
+++ javascript/custom2.js
@@ -5,3 +5,79 @@ var isPlaying = false;
 var sourceNode = null;
 var analyser = null;
 var theBuffer = null;
+var confidence = 0;
+var currentPitch = 0;
+
+window.onload = function() {
+	var request = new XMLHttpRequest();
+	request.open("GET", "./media/whistling3.ogg", true);
+	request.responseType = "arraybuffer";
+	request.onload = function() {
+	  audioContext.decodeAudioData( request.response, function(buffer) { 
+	    	theBuffer = buffer;
+		} );
+	}
+	request.send();
+}
+
+function error() {
+    alert('Stream generation failed.');
+}
+
+function getUserMedia(dictionary, callback) {
+    try {
+        navigator.getUserMedia = 
+        	navigator.getUserMedia ||
+        	navigator.webkitGetUserMedia ||
+        	navigator.mozGetUserMedia;
+        navigator.getUserMedia(dictionary, callback, error);
+    } catch (e) {
+        alert('getUserMedia threw exception :' + e);
+    }
+}
+
+function gotStream(stream) {
+    // Create an AudioNode from the stream.
+    var mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+    // Connect it to the destination.
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+    mediaStreamSource.connect( analyser );
+    updatePitch();
+}
+
+function toggleLiveInput() {
+    getUserMedia({audio:true}, gotStream);
+}
+
+function togglePlayback() {
+    var now = audioContext.currentTime;
+
+    if (isPlaying) {
+        //stop playing and return
+        sourceNode.stop( now );
+        sourceNode = null;
+        analyser = null;
+        isPlaying = false;
+		if (!window.cancelAnimationFrame)
+			window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
+        window.cancelAnimationFrame( rafID );
+        return "start";
+    }
+
+    sourceNode = audioContext.createBufferSource();
+    sourceNode.buffer = theBuffer;
+    sourceNode.loop = true;
+
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+    sourceNode.connect( analyser );
+    analyser.connect( audioContext.destination );
+    sourceNode.start( now );
+    isPlaying = true;
+    isLiveInput = false;
+    updatePitch();
+
+    return "stop";
+}
