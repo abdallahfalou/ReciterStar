8ee9e646e9f2eea766190bf7ca4526751e27871b
Abdallah El-Falou <abdallah.elfalou@gmail.com>
2014-02-06 01:27:11 -0500

    add Afasy recitation & fix pitch peaking issue

--- javascript/custom2.js
+++ javascript/custom2.js
@@ -2,15 +2,31 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
 var audioContext = new AudioContext();
 var isPlaying = false;
+var isRecording = false;
 var sourceNode = null;
 var analyser = null;
 var theBuffer = null;
+var detectorElem, 
+	canvasContext,
+	pitchElem,
+	noteElem,
+	detuneElem,
+	confidenceElem,
+	detuneAmount;
+var WIDTH=300;
+var CENTER=150;
+var HEIGHT=42;
 var confidence = 0;
 var currentPitch = 0;
 
+var start = new Date().getTime();
+var freq = new TimeSeries()
+var confidenceTimeSeries = new TimeSeries();
+
 window.onload = function() {
 	var request = new XMLHttpRequest();
-	request.open("GET", "./media/whistling3.ogg", true);
+	// request.open("GET", "./media/whistling3.ogg", true);
+	request.open("GET", "./media/001.mp3", true);
 	request.responseType = "arraybuffer";
 	request.onload = function() {
 	  audioContext.decodeAudioData( request.response, function(buffer) { 
@@ -18,6 +34,37 @@ window.onload = function() {
 		} );
 	}
 	request.send();
+	
+	detectorElem = document.getElementById( "detector" );
+	pitchElem = document.getElementById( "pitch" );
+	noteElem = document.getElementById( "note" );
+	detuneElem = document.getElementById( "detune" );
+	confidenceElem = document.getElementById( "confidence" );
+	detuneAmount = document.getElementById( "detune_amt" );
+	canvasContext = document.getElementById( "output" ).getContext("2d");
+
+	detectorElem.ondragenter = function () { 
+		this.classList.add("droptarget"); 
+		return false; };
+	detectorElem.ondragleave = function () { this.classList.remove("droptarget"); return false; };
+	detectorElem.ondrop = function (e) {
+  		this.classList.remove("droptarget");
+  		e.preventDefault();
+		theBuffer = null;
+
+	  	var reader = new FileReader();
+	  	reader.onload = function (event) {
+	  		audioContext.decodeAudioData( event.target.result, function(buffer) {
+	    		theBuffer = buffer;
+	  		}, function(){alert("error loading!");} ); 
+
+	  	};
+	  	reader.onerror = function (event) {
+	  		alert("Error: " + reader.error );
+		};
+	  	reader.readAsArrayBuffer(e.dataTransfer.files[0]);
+	  	return false;
+	};
 }
 
 function error() {
@@ -49,6 +96,7 @@ function gotStream(stream) {
 
 function toggleLiveInput() {
     getUserMedia({audio:true}, gotStream);
+	isRecording = true;
 }
 
 function togglePlayback() {
@@ -76,8 +124,144 @@ function togglePlayback() {
     analyser.connect( audioContext.destination );
     sourceNode.start( now );
     isPlaying = true;
-    isLiveInput = false;
+    isRecording = false;
     updatePitch();
 
     return "stop";
 }
+
+var rafID = null;
+var tracks = null;
+var buflen = 2048;
+var buf = new Uint8Array( buflen );
+var MINVAL = 134;  // 128 == zero.  MINVAL is the "minimum detected signal" level.
+
+var noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+function noteFromPitch( frequency ) {
+	var noteNum = 12 * (Math.log( frequency / 440 )/Math.log(2) );
+	return Math.round( noteNum ) + 69;
+}
+
+function frequencyFromNoteNumber( note ) {
+	return 440 * Math.pow(2,(note-69)/12);
+}
+
+function centsOffFromPitch( frequency, note ) {
+	return ( 1200 * Math.log( frequency / frequencyFromNoteNumber( note ))/Math.log(2) );
+}
+
+function autoCorrelate( buf, sampleRate ) {
+	var MIN_SAMPLES = 4;	// corresponds to an 11kHz signal
+	var MAX_SAMPLES = 1000; // corresponds to a 44Hz signal
+	var SIZE = 1000;
+	var best_offset = -1;
+	var best_correlation = 0;
+	var rms = 0;
+
+	confidence = 0;
+	currentPitch = 0;
+
+	if (buf.length < (SIZE + MAX_SAMPLES - MIN_SAMPLES))
+		return;  // Not enough data
+
+	for (var i=0;i<SIZE;i++) {
+		var val = (buf[i] - 128)/128;
+		rms += val*val;
+	}
+	rms = Math.sqrt(rms/SIZE);
+
+	for (var offset = MIN_SAMPLES; offset <= MAX_SAMPLES; offset++) {
+		var correlation = 0;
+
+		for (var i=0; i<SIZE; i++) {
+			correlation += Math.abs(((buf[i] - 128)/128)-((buf[i+offset] - 128)/128));
+		}
+		correlation = 1 - (correlation/SIZE);
+		if (correlation > best_correlation) {
+			best_correlation = correlation;
+			best_offset = offset;
+		}
+	}
+	if ((rms>0.01)&&(best_correlation > 0.01)) {
+		confidence = best_correlation * rms * 10000;
+		currentPitch = sampleRate/best_offset;
+		// console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")")
+	}
+	if (currentPitch == 11025) confidence = 0; // algorithm peaks at 11025 when audio isn't clear?
+}
+
+function updatePitch( time ) {
+	var cycles = new Array;
+	analyser.getByteTimeDomainData( buf );
+
+	// possible other approach to confidence: sort the array, take the median; go through the array and compute the average deviation
+	autoCorrelate( buf, audioContext.sampleRate );
+
+	confidenceElem.innerText = confidence;
+ 	if (confidence <10) {
+ 		detectorElem.className = "vague";
+	 	pitchElem.innerText = "--";
+		noteElem.innerText = "-";
+		detuneElem.className = "";
+		detuneAmount.innerText = "--";
+ 	} else {
+	 	detectorElem.className = "confident";
+	 	pitchElem.innerText = Math.floor( currentPitch ) ;
+	 	var note =  noteFromPitch( currentPitch );
+		noteElem.innerHTML = noteStrings[note%12];
+		var detune = centsOffFromPitch( currentPitch, note );
+		if (detune == 0 ) {
+			detuneElem.className = "";
+			detuneAmount.innerHTML = "--";
+
+			// TODO: draw a line.
+		} else {
+			if (Math.abs(detune)<10)
+				canvasContext.fillStyle = "green";
+			else
+				canvasContext.fillStyle = "red";
+
+			if (detune < 0) {
+	  			detuneElem.className = "flat";
+			}
+			else {
+				detuneElem.className = "sharp";
+			}
+  			canvasContext.fillRect(CENTER, 0, (detune*3), HEIGHT);
+			detuneAmount.innerHTML = Math.abs( Math.floor( detune ) );
+		}
+	}
+
+	if (!window.requestAnimationFrame)
+		window.requestAnimationFrame = window.webkitRequestAnimationFrame;
+	rafID = window.requestAnimationFrame( updatePitch );
+}
+
+// TODO: this is where TimeSeries are updated
+setInterval(function(){ 
+	confidenceTimeSeries.append(new Date().getTime(), confidence);
+	if ((isRecording || isPlaying) && confidence > 10) {
+		freq.append(new Date().getTime(), currentPitch);
+	} else {
+		freq.append(new Date().getTime(), 0);
+	}
+}, 1/audioContext.sampleRate*1000);
+
+function createTimeline() {
+    var gy_min = 0;
+    var gy_max = 250;
+
+    var chart_gy = new SmoothieChart({millisPerPixel: 12, grid: {fillStyle: '#ffffff', strokeStyle: '#f4f4f4', sharpLines: true, millisPerLine: 5000, verticalSections: 5}, timestampFormatter: SmoothieChart.timeFormatter, minValue: gy_min, maxValue: gy_max, labels:{fillStyle:'#000000'}});
+
+    chart_gy.addTimeSeries(freq, {lineWidth: 2, strokeStyle: 'black', fillStyle:'rgba(0, 0, 0, 0.3)'});
+	// chart_gy.addTimeSeries(confidenceTimeSeries, {strokeStyle: 'pink'});
+    chart_gy.streamTo(document.getElementById("freq-chart"));
+
+}
+
+function errorCallback() {
+	console.log("called errorCallback()");
+}
+
+createTimeline();
