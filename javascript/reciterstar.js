6016270f00dea81268dde79338e66488bf194ba1
Yaman-Aln <yaman.aln@gmail.com>
2014-03-15 23:49:16 -0400

    Delay in Matching
    Now the mic tracker starts plotting at t-999. There are problems when
    you have continuous input. I can't tell if that's the old tone
    algorithm. Will wait on YIN test, but this shows the idea anyhow.

--- javascript/reciterstar.js
+++ javascript/reciterstar.js
@@ -17,12 +17,15 @@ var CENTER=143;
 var HEIGHT=42;
 var confidence = 0;
 var currentPitch = 0;
+var isReciting = false;
 
 var templatePitchIndex = 0;
 var templatePitchArray = [65,69,94,94,94,93,93,93,93,93,93,93,93,93,93,93,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,150,85,86,74,86,86,62,58,86,86,74,58,86,86,86,66,67,86,74,86,86,86,150,67,67,85,85,150,150,150,150,150,150,150,150,150,150,150,92,150,93,69,74,65,81,81,59,81,81,81,81,81,81,74,81,81,74,57,59,65,93,150,150,150,150,150,150,86,86,86,86,86,86,67,74,86,74,86,67,86,62,86,86,86,86,86,86,86,86,74,58,86,85,150,150,150,150,150,150,92,92,92,92,92,92,93,92,93,93,65,93,65,57,69,93,74,93,93,93,93,93,93,150,150,150,150,150,150,150,86,87,87,56,87,87,87,150,150,150,87,68,87,87,63,87,87,87,87,87,87,56,63,150,150,74];
+var startPitch = [150,150]
 // var templateSampleRate = 22050;
 
 var testPitchArray = new Array();
+
 var pitchDiff = new Array();
 var FreqUpdate=5;
 
@@ -31,6 +34,7 @@ var v = document.getElementsByTagName("video")[0]
 var start = new Date().getTime();
 var testPitchTS = new TimeSeries();
 var templatePitchTS = new TimeSeries();
+var startPitch = new TimeSeries();
 
 window.onload = function() {
 	var request = new XMLHttpRequest();
@@ -108,6 +112,7 @@ function gotStream(stream) {
 
 function toggleLiveInput() {
     getUserMedia({audio:true}, gotStream);
+	isReciting=true 
 }
 
 
@@ -288,19 +293,22 @@ function updatePitch( time ) {
 // TODO: this is where TimeSeries are updated
 setInterval(function(){ 
 	var t = new Date().getTime();
-	
+	//var s = new Date().getTime();	
+	//startPitch.append(s-999, 50);
+	//s=s-1
 	if (templatePitchIndex === templatePitchArray.length) {
 		templatePitchIndex = 0;
 		console.log("templatePitchIndex reset");
 	}
-	if (isPlaying) {
+	if (isPlaying || isReciting) {
 		templatePitchTS.append(t, templatePitchArray[templatePitchIndex++]);
+			
 	} else {
 		templatePitchTS.append(t, 0);
 	}
 	if (confidence > 10) {
-		testPitchTS.append(t, currentPitch);
-	} else {
+		testPitchTS.append(t-1000, currentPitch);
+			} else {
 		testPitchTS.append(t, 0);
 	}
 }, 25);
@@ -311,7 +319,8 @@ function createTimeline() {
 
     var chart_gy = new SmoothieChart({millisPerPixel: 12, grid: {fillStyle: '#ffffff', strokeStyle: '#f4f4f4', sharpLines: true, millisPerLine: 5000, verticalSections: 5}, timestampFormatter: SmoothieChart.timeFormatter, minValue: gy_min, maxValue: gy_max, labels:{fillStyle:'#000000'}});
 
-    chart_gy.addTimeSeries(testPitchTS, {lineWidth: 2, strokeStyle: 'black', fillStyle:'rgba(0, 0, 0, 0.3)'});
+    chart_gy.addTimeSeries(testPitchTS, { strokeStyle:'rgb(0, 255, 0)',  lineWidth:3 });
+	//chart_gy.addTimeSeries(startPitch, {lineWidth: 10, strokeStyle: 'blue', fillStyle:'rgba(0, 0, 0, 5)'});
     chart_gy.addTimeSeries(templatePitchTS, {lineWidth: 2, strokeStyle: 'red', fillStyle:'rgba(0, 0, 0, 0.3)'});
     chart_gy.streamTo(document.getElementById("pitch-chart"));
 
